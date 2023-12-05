c12d846e79f2fb05d262f3fb13bfc049cefc3df0
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2023-12-05 20:34:40 +0900

    ルートパスの定義、viewファイル並びにCSSファイル配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :items, only: :index
+  root to: 'items#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
