5258f63b0e5dcbfc6e34b99d6a0b3c80d2e7fdfa
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2023-12-10 00:46:07 +0900

    モデル作成、アソシエーション、エラーメッセージ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+  
   private
 
   def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  # これで ActiveHashのアソシエーション
   belongs_to :user
   has_one :orderend
+  # これでテーブルのアソシエーション
   has_one_attached :image
+  # これでActiveStorageのアソシエーション
+
+  # カテゴリの選択「---」の時に保存できないようにするバリデーション
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
