db3b828a7f387fdef1c979cca1f9ea52e54fd0ec
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2023-12-11 10:35:47 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,10 +10,22 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      # 保存成功の処理
+      redirect_to root_path
+    else
+      # バリデーションが通過しない場合の処理
+      render :new
+    end
+  end
+
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
